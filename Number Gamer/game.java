56ea3cc72d8816cddf768cde031842c4d8ad23b2
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-04-28 22:28:11 +0530

    Update game.java

--- Number Gamer/game.java
+++ Number Gamer/game.java
@@ -1,1 +1,28 @@
-dadssaf
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessNum {
+    public static void main(String[] args) {
+        Random rndm = new Random();
+        int rndmNum = rndm.nextInt(100) + 1; 
+
+        Scanner scanner = new Scanner(System.in);
+        int userinput;
+
+        System.out.println("Guess the num (btw 1 and 100):");
+
+        do {
+            userinput = scanner.nextInt();
+
+            if (userinput < rndmNum) {
+                System.out.println("Too low! Try again.");
+            } else if (userinput > rndmNum) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congrats! U guessed the correct num, " + rndmNum + ".");
+            }
+        } while (userinput != rndmNum);
+
+        scanner.close();
+    }
+}
